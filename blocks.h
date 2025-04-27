8ea2d74344c6bad351594dcd3aadbc73df464e33
Kisbogyi <kisbogyi2003@gmail.com>
2025-04-27 10:53:39 +0200

    change icons

--- blocks.h
+++ blocks.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/	/*Command*/											/*Update Interval*/	/*Update Signal*/
-    {"💡 ",		"echo \"$(brightnessctl -P g)\"%",										10,	0},
-    {"⌨️ ",		"setxkbmap -query | grep layout | sed 's/layout: *//g'",							120,	0},
-    {"💾 ",		"df -h / | awk 'NR==2 {printf \"Porn folder: %dG/%dG\", $3, $2}'",							120,	0},
-    {"🔋",		"upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'percentage:'| sed 's/ *percentage: *//'",	30,	0},
+    {"",		"echo \"$(brightnessctl g)\"%",										10,	0},
+    {"",		"setxkbmap -query | grep layout | sed 's/layout: *//g'",							120,	0},
+    {"",		"df -h / | awk 'NR==2 {printf \"Porn folder: %dG/%dG\", $3, $2}'",							120,	0},
+    {"󰁹",		"upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'percentage:'| sed 's/ *percentage: *//'",	30,	0},
     {"",		"date '+%b %d (%a) %I:%M%p'",											5,	0},
 };
 
