4adb74322a394d4bef8274ab5213bccc664b26b0
Kisbogyi <kisbogyi2003@gmail.com>
2025-05-01 16:40:04 +0200

    better output

--- blocks.h
+++ blocks.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/	/*Command*/											/*Update Interval*/	/*Update Signal*/
-    {" ",		"echo \"$(brightnessctl g)\"%",										10,	0},
-    {"  ",		"setxkbmap -query | grep layout | sed 's/layout: *//g'",							120,	0},
-    {" ",		"df -h / | awk 'NR==2 {printf \"Porn folder: %dG/%dG\", $3, $2}'",							120,	0},
-    {"󰁹 ",		"cat /sys/class/power_supply/BAT0/capacity",	30,	0},
-    {"",		"date '+%b %d (%a) %I:%M%p'",											5,	0},
+    {" ",		"brightnessctl -m | awk -F , '{print $4}'",						10,	0},
+    {"  ",		"setxkbmap -query | grep layout | sed 's/layout: *//g'",				120,	0},
+    {" ",		"df -h / | awk 'NR==2 {printf \"Porn folder: %dG/%dG\", $3, $2}'",			120,	0},
+    {"󰁹 ",		"echo \"$(cat /sys/class/power_supply/BAT0/capacity)\"%",				60,	0},
+    {"",		"date '+%b %d (%a) %I:%M%p'",								60,	0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
